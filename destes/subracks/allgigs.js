52816bcb43f89c1e3ac1a345502f45b9edf7b015
Javier Barrios <73854785+MuayThaiLegz@users.noreply.github.com>
2022-06-30 11:29:45 -0700

    modifying to needs
    looking to disrupt

---
+++ destes/subracks/allgigs.js
@@ -0,0 +1,14 @@
+import Gigs1 from './Gigs1';
+import Gigs2 from './Gigs2';
+import Gigs3 from './Gigs3';
+import Gigs4 from './Gigs4';
+
+
+const RGigs = [
+    Gigs1,
+	Gigs2,
+	Gigs3,
+	Gigs4,
+	
+];
+export default RGigs ;
